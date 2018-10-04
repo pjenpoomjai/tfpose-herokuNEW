2346295638e7a33dbd2d425cd8a6cf7bb4008858
pjenpoomjai <pjenpoomjai@gmail.com>
2018-10-04 23:41:09 +0700

    commit

--- src/processImage.py
+++ src/processImage.py
@@ -15,6 +15,7 @@ import time
 from estimator import TfPoseEstimator
 from networks import get_graph_path, model_wh
 from matplotlib import style
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import common
