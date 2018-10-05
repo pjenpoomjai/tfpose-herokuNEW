be95d5c9d97e0446a2e5e00de3c128de54833668
pjenpoomjai <pjenpoomjai@gmail.com>
2018-10-05 22:58:52 +0700

    fix

--- src/deploy.py
+++ src/deploy.py
@@ -8,21 +8,20 @@ from processImage import Terrain
 #app = Flask(__name__)
 
 #@app.route('/')
-def index():
-    def on_connect(client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+        
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-        client.subscribe("zenbo/image")
-    def on_message(client, userdata, msg):
-        print ("Topic : ", msg.topic)
-        f = open("./images/tet.jpg", "wb")  #there is a output.jpg which is different
-        f.write(msg.payload)
-        f.close()
-        print('received image')
-        processImage()
-
-    run = True
+    client.subscribe("zenbo/image")
+def on_message(client, userdata, msg):
+    print ("Topic : ", msg.topic)
+    f = open("./images/tet.jpg", "wb")  #there is a output.jpg which is different
+    f.write(msg.payload)
+    f.close()
+    print('received image')
+    processImage()
+def run():
     broker_address = "iot.eclipse.org"
     client.on_connect = on_connect
     client.on_message = on_message  # attach function to callback
@@ -44,5 +43,5 @@ def processImage():
 if __name__ == "__main__":
     t = Terrain()
     print("creating new instance")
-    client = mqtt.Client('client')  # create new instance
-    index()
+    client = mqtt.Client('cloudPRocess')  # create new instance
+    run()
